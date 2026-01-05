73942e108567aff157e7100ca7188e9ab109ce12
Pham Quoc Khanh <khanhphamquocnb@gmail.com>
2026-01-05 16:28:34 +0700

    Update Category api

--- server/models/category.model.js
+++ server/models/category.model.js
@@ -22,5 +22,5 @@ const categorySchema = mongoose.Schema({
 }, { timestamps: true });
 
 const CategoryModel = mongoose.model('Category', categorySchema)
-
+// export default CategoryModel
 export default CategoryModel
