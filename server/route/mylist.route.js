57d98449d283962d6bf90dfd1790f230c793cdb7
Pham Quoc Khanh <khanhphamquocnb@gmail.com>
2026-01-05 16:25:01 +0700

    Updated myList_api

--- server/route/mylist.route.js
+++ server/route/mylist.route.js
@@ -4,12 +4,12 @@ import auth from '../middlewares/auth.js';
 import { addToMyListController, deleteToMyListController, getToMyListController } from '../controllers/mylist.controller.js';
 
 const myListRouter = Router()
-
+// Test API Postman
 myListRouter.post('/add', auth, addToMyListController);
 myListRouter.delete('/remove/:id', auth, deleteToMyListController);
 myListRouter.get('/get', auth, getToMyListController);
 
 
 
-
+// export default myListRouter
 export default myListRouter;
